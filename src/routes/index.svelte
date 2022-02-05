ef4a042cf144f723b107a50972e363017acee52b
Mira <athes01@gmail.com>
2022-02-05 14:41:23 +0100

    configured svelte and tailwind

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<Layout>
+    <h1 class="text-xl font-bold">Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</Layout>
+
+<script>
+    import Layout from './__layout.svelte'
+</script>
