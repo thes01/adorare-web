acdce91bfb4cf0b367a143ff7807bee6632152e5
Mira <athes01@gmail.com>
2022-05-15 13:17:35 +0200

    fixed assets loading

--- src/routes/components/organisms/MainLogo.svelte
+++ src/routes/components/organisms/MainLogo.svelte
@@ -1,11 +1,11 @@
 <div data-tilt class="tilt-layer-base sizing-box flex justify-center items-center">
-    <img src="../static/design/3.min.png" alt="" class="tilt-layer-1 sizing-box">
-    <img src="../static/design/2.min.png" alt="" class="tilt-layer-2 sizing-box">
-    <img src="../static/design/4.min.png" alt="" class="tilt-layer-3 sizing-box">
-    <img src="../static/design/5.min.png" alt="" class="tilt-layer-3 sizing-box">
-    <img src="../static/design/7.min.png" alt="" class="tilt-layer-4 sizing-box">
-    <img src="../static/design/1.min.png" alt="" class="tilt-layer-5 sizing-box">
-    <img src="../static/adologo_newgen.svg" alt="" class="tilt-layer-6 w-2/5">
+    <img src="/design/3.min.png" alt="" class="tilt-layer-1 sizing-box">
+    <img src="/design/2.min.png" alt="" class="tilt-layer-2 sizing-box">
+    <img src="/design/4.min.png" alt="" class="tilt-layer-3 sizing-box">
+    <img src="/design/5.min.png" alt="" class="tilt-layer-3 sizing-box">
+    <img src="/design/7.min.png" alt="" class="tilt-layer-4 sizing-box">
+    <img src="/design/1.min.png" alt="" class="tilt-layer-5 sizing-box">
+    <img src="/adologo_newgen.svg" alt="" class="tilt-layer-6 w-2/5">
 </div>
 
 <!-- <img src="../static/design/static.png" alt="static logo"> -->
@@ -32,7 +32,3 @@
 .tilt-layer-5 { transform: translateZ(theme('spacing.8')); }
 .tilt-layer-6 { transform: translateZ(theme('spacing.10')); }
 </style>
-
-<script>
-    
-</script>
