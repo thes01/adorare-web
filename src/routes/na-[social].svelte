98c7c9d983f342e3d5ee3fb25abde4e07634b906
Mira <athes01@gmail.com>
2022-05-15 16:40:27 +0200

    implemented redirecting

---
+++ src/routes/na-[social].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+    // TODO: fetch the map from backend..?
+    const map = {
+        'facebooku': 'https://www.facebook.com/AdorareVK/',
+        'facebook': 'https://www.facebook.com/AdorareVK/',
+        'fb': 'https://www.facebook.com/AdorareVK/',
+        'instagramu': 'https://www.instagram.com/skupina_adorare/',
+        'instagram': 'https://www.instagram.com/skupina_adorare/',
+        'ig': 'https://www.instagram.com/skupina_adorare/',
+        'spotify': 'https://open.spotify.com/artist/3vnsh4Z8eBN0AD6iTiITn3',
+        'youtube': 'https://www.youtube.com/channel/UCN150yOV_e0l9pev3soXP_g',
+        'ytb': 'https://www.youtube.com/channel/UCN150yOV_e0l9pev3soXP_g'
+    }
+
+    export async function load( { params } ) {
+        const socialId = params.social
+        const redirectUri = map[socialId] || '/'
+        console.log(`Redirecting to ${redirectUri}.`)
+
+        return {
+            status: 302,
+            redirect: redirectUri
+        };
+    }
+</script>
