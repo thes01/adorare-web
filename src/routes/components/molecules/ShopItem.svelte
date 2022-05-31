46af7bb78ec11dcbd7aa5b204617b5e32378abd8
Mira <athes01@gmail.com>
2022-05-31 21:01:14 +0200

    added first shop components

---
+++ src/routes/components/molecules/ShopItem.svelte
@@ -0,0 +1,67 @@
+<div class="flex items-center w-full -my-14">
+    <div class="h-32 overflow-visible">
+        <img src="{iconSrc}" alt="{iconAlt}" class="relative w-48 mr-6" style="top: {topOffset}px; left: {leftOffset}px;">
+    </div>
+    <div class="grow">
+        <h3 class="mr-6"><slot></slot></h3>
+    </div>
+    {#if price}
+        <div class="text-navy-blue opacity-60 text-center mr-4 pt-2">{price} Kč</div>
+    {/if}
+    <div class="inline-flex pt-2">
+        {#if !unavailable}
+            <button on:click="{qtyHelper.sub}">-</button>
+            <span contenteditable 
+                class="input px-1 mx-2 border-none text-right focus:outline-navy-blue"
+                role="textbox"
+                bind:innerHTML="{qtyHelper.qtyStr}"
+                on:keypress="{digitPass}">
+            </span>
+            <span class="-ml-1 mr-2">ks</span>
+            <button on:click="{qtyHelper.add}">+</button>
+        {:else}
+            <a class="text-navy-blue opacity-60 no-underline hover:underline" href="{unavailableLink}">{unavailable}</a>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .input {
+        min-width: 1rem;
+        width: auto;
+    }
+</style>
+
+<script lang="ts">
+    export let iconSrc: string;
+    export let iconAlt: string;
+    export let price: number|null = null;
+    export let qty: number = 0;
+    export let topOffset: number = 0;
+    export let leftOffset: number = 0;
+    export let unavailable: string = "";
+    export let unavailableLink: string = "#";
+    
+    $: qtyHelper = {
+        get qtyStr() {
+            return `${qty}`
+        },
+        set qtyStr(val: string) {
+            qty = parseInt(val) || 0
+        },
+        add() {
+            qty += 1;
+        },
+        sub() {
+            qty = Math.max(0, qty - 1);
+        }
+    }
+
+    // pass only digits (and delete also)
+    const digitPass = (event) => {
+        const is_n = (/\d/.test(String.fromCharCode(event.which) ));
+        if (!is_n) {
+            event.preventDefault();
+        }
+    }
+</script>
---
+++ src/routes/components/organisms/Shop.svelte
@@ -0,0 +1,58 @@
+<div>
+    <div class="w-1/2 my-16">
+        <ShopItem
+            iconSrc="/shop/k-vodam.png"
+            iconAlt="CD K vodám"
+            bind:qty={orderQties.k_vodam}
+            price={250}
+            topOffset={17}>
+            K vodám
+        </ShopItem>
+        <ShopItem
+            iconSrc="/shop/new-generation.png"
+            iconAlt="CD New generation"
+            bind:qty={orderQties.new_generation}
+            price={200}
+            topOffset={-8}>
+            New generation
+        </ShopItem>
+        <ShopItem
+            iconSrc="/shop/nech-se-vest.png"
+            iconAlt="CD Nech se vést"
+            bind:qty={orderQties.nech_se_vest}
+            price={200}
+            topOffset={2}>
+            Nech se vést
+        </ShopItem>
+        <ShopItem
+            iconSrc="/shop/v-otazkach.png"
+            iconAlt="CD V otázkách"
+            bind:qty={orderQties.v_otazkach}
+            price={200}
+            topOffset={-2}>
+            V otázkách
+        </ShopItem>
+        <ShopItem
+            unavailable="volně ke stažení"
+            iconSrc="/shop/volame-k-tobe.png"
+            iconAlt="CD Voláme k Tobě"
+            topOffset={8}
+            leftOffset={7}>
+            Voláme k tobě
+        </ShopItem>
+    </div>
+
+    <!-- todo: add Splide or some other carousely-thing for images -->
+    <!-- https://splidejs.com/integration/svelte-splide/ -->
+</div>
+
+<script>
+    import ShopItem from '../molecules/ShopItem.svelte'
+
+    $: orderQties = {
+        k_vodam: 0,
+        new_generation: 0,
+        nech_se_vest: 0,
+        v_otazkach: 0
+    }
+</script>
