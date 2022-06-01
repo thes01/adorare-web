c1891f988a65e2467e96534c3a864ca63f8a8fc2
Mira <athes01@gmail.com>
2022-06-01 11:21:48 +0200

    changed button font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,8 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
 </svelte:head>
   
 
--- src/routes/components/atoms/Button.svelte
+++ src/routes/components/atoms/Button.svelte
@@ -1,4 +1,4 @@
-<button class="bg-navy-blue text-white py-2 px-4">
+<button class="bg-navy-blue text-white py-2 px-4 uppercase font-work-sans text-sm tracking-wider">
     <slot></slot>
 </button>
 
