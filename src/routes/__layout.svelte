fb97c9a0452a83763ee22a5ac6e3dcbb7aa63f00
Mira <athes01@gmail.com>
2022-06-06 09:25:38 +0200

    removed old code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,11 @@
 <script>
     import "../styles/app.css";
-    import "flowbite/dist/flowbite.css";
+    // import "flowbite/dist/flowbite.css";
 </script>
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&display=swap" rel="stylesheet"> -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
