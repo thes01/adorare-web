f162c2d7f147f26b25c894a96da04f8e6f0e920a
Mira <athes01@gmail.com>
2022-06-06 08:19:50 +0200

    smaller updates

--- src/routes/data/GalleryImages.js
+++ src/routes/data/GalleryImages.js
@@ -1,25 +1,27 @@
 class Img {
-    constructor(url) {
-        this.url = url
+    constructor(uri) {
+        this.uri = uri
+        this.base_url = 'https://res.cloudinary.com/adorare/image/upload/'
     }
 
     get thumbUrl() {
-        return this.url
+        const transformation = 'c_scale,q_65,w_1794'
+        return this.base_url + transformation + '/' + this.uri
     }
 
     get fullUrl() {
-        return this.url
+        return this.base_url + '/' + this.uri
     }
 }
 
 export default {
     Img,
     ImageList: [
-        new Img('https://res.cloudinary.com/adorare/image/upload/v1654423974/ado-merch/IMG_0402_1_sf7ebo.jpg'),
-        new Img('https://res.cloudinary.com/adorare/image/upload/v1654423979/ado-merch/IMG_0460_1_imhfqt.jpg'),
-        new Img('https://res.cloudinary.com/adorare/image/upload/v1654423980/ado-merch/IMG_0472_1_myjme9.jpg'),
-        new Img('https://res.cloudinary.com/adorare/image/upload/v1654423979/ado-merch/IMG_0459_1_vz5h7k.jpg'),
-        new Img('https://res.cloudinary.com/adorare/image/upload/v1654423976/ado-merch/IMG_0424_1_jkikmc.jpg'),
-        new Img('https://res.cloudinary.com/adorare/image/upload/v1654423972/ado-merch/IMG_0379_1_vjehsb.jpg'),
+        new Img('v1654423974/ado-merch/IMG_0402_1_sf7ebo.jpg'),
+        new Img('v1654423979/ado-merch/IMG_0460_1_imhfqt.jpg'),
+        new Img('v1654423980/ado-merch/IMG_0472_1_myjme9.jpg'),
+        new Img('v1654423979/ado-merch/IMG_0459_1_vz5h7k.jpg'),
+        new Img('v1654423976/ado-merch/IMG_0424_1_jkikmc.jpg'),
+        new Img('v1654423972/ado-merch/IMG_0379_1_vjehsb.jpg'),
     ]
 }
