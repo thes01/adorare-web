1bf63ede00c8c3a6881b2770195f30b1113b13a0
Mira <athes01@gmail.com>
2022-06-06 09:47:33 +0200

    fixed mobile wrapping in shop

--- src/routes/components/molecules/ShopItem.svelte
+++ src/routes/components/molecules/ShopItem.svelte
@@ -1,20 +1,23 @@
-<div class="flex items-center w-full -my-14">
+<div class="flex items-center w-full -my-10 md:-my-14">
     <div class="h-32 shrink-0 shop-item-icon overflow-hidden">
         <img src="{iconSrc}" alt="{iconAlt}" class="relative w-48 mr-6 max-w-none select-none" style="top: {topOffset}px; left: {leftOffset}px;">
     </div>
-    <div class="grow shrink-0">
-        <h3 class="mr-6 my-6"><slot></slot></h3>
-    </div>
-    {#if price}
-        <div class="text-navy-blue opacity-60 text-center mr-4 pt-2">{price} Kč</div>
-    {/if}
-    <div>
-        <div class="inline-flex pt-2">
-            {#if !unavailable}
-                <ShopItemQty inputName={unavailable ? null : itemId} bind:qty></ShopItemQty>
-            {:else}
-                <a class="text-navy-blue opacity-60 no-underline hover:underline" href="{unavailableLink}">{unavailable}</a>
+    <div class="flex items-center w-full flex-wrap
+            xs:my-12 md:my-6">
+        <div class="grow shrink-0">
+            <h3 class="mr-6 leading-10"><slot></slot></h3>
+        </div>
+        <div class="flex">
+            {#if price}
+                <div class="text-navy-blue sm:opacity-60 text-center mr-4 pt-2 shrink-0">{price} Kč</div>
             {/if}
+            <div class="inline-flex pt-2">
+                {#if !unavailable}
+                    <ShopItemQty inputName={unavailable ? null : itemId} bind:qty></ShopItemQty>
+                {:else}
+                    <a class="text-navy-blue sm:opacity-60 no-underline hover:underline" href="{unavailableLink}">{unavailable}</a>
+                {/if}
+            </div>
         </div>
     </div>
 </div>
