ed8373abb80091597911f5927e0eea89a847aafd
Mira <athes01@gmail.com>
2022-08-30 14:52:12 +0200

    modified timeline to a wrapper

---
+++ src/routes/components/molecules/TimelineWrapper.svelte
@@ -0,0 +1,16 @@
+<div class={`relative ${cls}`}>
+    <div class="timeline-line timeline-fader-top right-10"
+        style={`height: ${topPx}px; top: -${topPx}px;`}></div>
+    <div class="timeline-line timeline-fader-bottom right-10"
+        style={`height: ${bottomPx}px; bottom: -${bottomPx}px;`}></div>
+    <div class="timeline-dot -top-16"></div>
+
+    <slot></slot>
+</div>
+
+
+<script>
+    export let topPx = 200;
+    export let bottomPx = 200;
+    export let cls = '';
+</script>
