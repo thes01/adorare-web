c9674dff07ffbda94ad11964fe300c090ab9a85f
Mira <athes01@gmail.com>
2022-08-30 13:35:08 +0200

    added thin nunito font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,7 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;600&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
     <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
